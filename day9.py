ec35e7ec95b3f4982ccf344568cd0889c9e76d09
Keven Kurz <kkurz@ctl.de>
2021-12-09 14:23:11 +0100

    aufgabe2 war einfach

--- day9.py
+++ day9.py
@@ -24,17 +24,25 @@ for ix, line in enumerate(input_data):
 print(sol)
 
 for bx, basin in enumerate(basins):
-    print(basin)
     while True:
         check = []
-        for point in basin:
+        for point in basins[bx]:
             for a in adj_m:
                 realx= point[0]+a[0]
                 realy = point[1]+a[1]
                 if realx >= 0 and realy >= 0 and realx < len(input_data) and realy < len(line):
-                    check.append((realx, realy))
-        check = list(filter(lambda x: x not in basins[bx], check))
-        print(check)
-        break
+                    if input_data[realx][realy] != "9":
+                        check.append((realx, realy))
+        check = list(filter(lambda x: x not in basins[bx], set(check)))
+        if len(check) == 0:
+            break
+        basins[bx] += check
 
 
+basins.sort(key=lambda x: len(x), reverse=True)
+
+for item in basins:
+    print(len(item))
+
+print("Aufgabe2")
+print(len(basins[0]) * len(basins[1]) *len(basins[2]))
