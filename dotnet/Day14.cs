fe8bf3935bf88cc21cef341c0a20a3beeaa034aa
Keven Kurz <kkurz@ctl.de>
2021-12-13 15:27:50 +0100

    vorbereiten 14

--- dotnet/Day14.cs
+++ dotnet/Day14.cs
@@ -4,8 +4,17 @@ class Day14
     {
         var test = File.ReadAllLines("day14.input");
         var numbers = test.Select(x => Convert.ToInt32(x)).ToArray();
+        var cities = new Dictionary<string, string>() {
+            {"UK", "London, Manchester, Birmingham"},
+            {"USA", "Chicago, New York, Washington"},
+            {"India", "Mumbai, New Delhi, Pune"}
+        };
+        (int, int)[] test2 = { (1, 2) };
 
-        Console.WriteLine("day14");
+        foreach ((var x, var y) in test2)
+        {
+            System.Console.WriteLine(x.ToString() + " " + y.ToString());
+        }
 
     }
 }
