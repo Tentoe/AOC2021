431195d08ea574a42840e486cebec467b6af9028
Keven Kurz <kkurz@ctl.de>
2021-12-29 17:08:06 +0100

    day18 iter

--- dotnet/Day18.cs
+++ dotnet/Day18.cs
@@ -1,4 +1,6 @@
 
+using System.Collections;
+
 class Day18
 {
     abstract class Element
@@ -40,8 +42,11 @@ class Day18
                 right = new Number(Convert.ToInt32(str[startRight].ToString()));
             else
                 right = Element.parse(cutEnd(str.Substring(startRight)));
-            return new Pair(left, right);
-            throw new Exception("First char not [");
+
+            var ret = new Pair(left, right);
+            if (ret == null) throw new Exception("Null parse");
+            return ret;
+
         }
 
 
@@ -62,7 +67,7 @@ class Day18
             return Value.ToString();
         }
     }
-    class Pair : Element
+    class Pair : Element, IEnumerable<Element>
     {
         public Pair(Element left, Element right)
         {
@@ -99,18 +104,59 @@ class Day18
 
             return rval || lval;
         }
+
+        public IEnumerator<Element> GetEnumerator()
+        {
+            var test = new List<Element>();
+
+            FindRec(test, this);
+
+            return test.GetEnumerator();
+        }
+
+        private void FindRec(List<Element> test, Element e)
+        {
+            if (e is Number)
+                return;
+
+            if (e is Pair p)
+            {
+                if (p.Left is Number && p.Right is Number)
+                {
+                    test.Add(e);
+                    return;
+                }
+                else
+                {
+                    FindRec(test, p.Left);
+                    FindRec(test, p.Right);
+                }
+            }
+
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
     }
     public static void main()
     {
         var read = new List<string>(File.ReadAllLines("day18.input"));
 
-        foreach (var item in read)
+        // foreach (var item in read)
+        // {
+        //     var test = Element.parse(item);
+        //     if (test is Pair t)
+        //         System.Console.WriteLine(Pair.CanExplode(t));
+        // }
+
+        var test2 = Element.parse(read[6]) as Pair;
+
+        foreach (var item in test2)
         {
-            var test = Element.parse(item);
-            if (test is Pair t)
-                System.Console.WriteLine(Pair.CanExplode(t));
+            System.Console.WriteLine(item);
         }
-
-
     }
 }
